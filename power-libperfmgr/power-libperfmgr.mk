33a42937e097355ad549fe61b7f75f7d1293a604
tuan <tuan94_bg@outlook.com>
2023-07-30 08:36:27 +0900

    import pixel power

---
+++ power-libperfmgr/power-libperfmgr.mk
@@ -0,0 +1,11 @@
+PRODUCT_SOONG_NAMESPACES += \
+	hardware/google/pixel \
+    hardware/google/interfaces
+
+PRODUCT_COPY_FILES += \
+	hardware/sony/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_PACKAGES += \
+	android.hardware.power-service.sony-libperfmgr \
+	libqti-perfd-client \
+	init.qcom.power.rc
