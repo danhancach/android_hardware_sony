04de923d9a19c26b30fb3f1ceef6351ae122a3d0
tuan <tuan94_bg@outlook.com>
2023-10-19 23:01:36 +0900

    healthd-ext: Add health HAL AIDL implementation
    Add health HAL AIDL implementation which is a service running for
    both health HAL and charger mode.
    
    healthd-ext: Add suspend support for charger mode
    
    healthd-ext: Update AIDL HAL service installation paths
    
    healthd-ext: update VINTF manifest version to 2
    
    healthd-ext: Rename hal to xiaomi to avoid compilation conflicts
    
    healthd-ext: Define override to QTI AIDL healthd-ext hal
    
    healthd-ext: Fix charge_counter and ETA values

---
+++ healthd-ext/healthd-ext.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.sony \
+    android.hardware.health-service.sony_recovery \
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
